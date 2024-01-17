a148bdfad159373239bc0691830f3b53b15d80f1
Olya Girnyak <olyagirnyak1999@gmail.com>
2024-01-17 18:04:39 +0200

    make build

---
+++ index.f65e8061.js
@@ -0,0 +1,3 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.querySelector("form");e.addEventListener("submit",function(n){n.preventDefault(),e.reset()});//# sourceMappingURL=index.f65e8061.js.map
+
+//# sourceMappingURL=index.f65e8061.js.map
